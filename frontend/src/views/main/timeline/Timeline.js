218470739fcb43274b1bf5b8ffdee8f598f69d72
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2020-11-25 22:24:16 +0200

    Feat: create the timeline component

---
+++ frontend/src/views/main/timeline/Timeline.js
@@ -0,0 +1,9 @@
+import React from "react";
+import classes from "./Timeline.module.scss";
+import cs from "classnames";
+
+const Timeline = ({ className }) => {
+  return <div className={cs(className)}></div>;
+};
+
+export default Timeline;
