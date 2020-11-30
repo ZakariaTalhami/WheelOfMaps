d129ff92a3fd55b1ed10144456b59dcee4d22c79
Zakaria Talhami <zakaria.a.a.talhami@gmail.com>
2020-11-30 22:11:55 +0200

    Feat: Create the timeline component

--- frontend/src/views/main/timeline/Timeline.js
+++ frontend/src/views/main/timeline/Timeline.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import classes from "./Timeline.module.scss";
 import cs from "classnames";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,6 +7,14 @@ import {
   setSelectedBook,
   setSelectedChapter,
 } from "../../../actions/BookActions";
+
+import { ReactComponent as TimePoint } from "../../../assets/icons/TimePoint.svg";
+import { ReactComponent as TimePointActive } from "../../../assets/icons/TimePointActive.svg";
+import useScrollToElement from "../../../hooks/useScrollToElement";
+import Tooltip from "../../../components/Tooltip/Tooltip";
+import ReactTooltip from "react-tooltip";
+import useHorizontalScroll from "../../../hooks/useHorizontalScroll";
+import useMouseDragScroll from "../../../hooks/useMouseDragScroll";
 
 const Timeline = ({ className }) => {
   const bookState = useSelector((state) => state.Books);
@@ -61,7 +69,7 @@ const Timeline = ({ className }) => {
       {/* Book and Chapter Labels / dropdowns */}
       <BookChapterBreadCrumb {...bookState} />
       {/* Timeline bar with the chapter points */}
-      <div></div>
+      <TimelineBar spacing={500} {...bookState} />
       {/* Back and next buttons */}
       <NavigationButtons back next onBack={handleBack} onNext={handleNext} />
     </div>
@@ -110,16 +118,84 @@ const BookChapterBreadCrumb = ({ books, selectedBook, selectedChapter }) => {
   );
 };
 
-const TimelineBar = () => {};
+const TimelineBar = ({ books, selectedBook, selectedChapter, spacing }) => {
+  const chapters = books[selectedBook];
+  const barLength = spacing * (chapters.length - 1);
+  const dispatch = useDispatch();
+  const activeElementRef = useScrollToElement([selectedChapter, selectedBook]);
+  const timelineContainerRef = useRef(null);
+  useHorizontalScroll(timelineContainerRef);
+  useMouseDragScroll(timelineContainerRef);
+
+  // When clickon on a chapter, the tooltip becomes stuck,
+  // this will rebuild the tooltip when a new chapter is selected
+  useEffect(() => {
+    ReactTooltip.rebuild();
+  });
+
+  const PlaceholderMarkers = ({ position, selected, title, ...props }) => {
+    return (
+      <div
+        className={cs(classes.marker)}
+        style={{ left: position }}
+        ref={selected ? activeElementRef : null}
+        {...props}
+      >
+        {selected ? (
+          <TimePointActive data-tip={title} />
+        ) : (
+          <TimePoint data-tip={title} />
+        )}
+      </div>
+    );
+  };
+
+  const handleSelection = (chapter, index) => {
+    dispatch(setSelectedChapter(chapter, index));
+  };
+
+  return (
+    <div ref={timelineContainerRef} className={classes.timelineBarContainer}>
+      <div className={classes.timelineBarWrapper}>
+        <div
+          className={classes.timelineBar}
+          style={{
+            width: `${barLength}px`,
+          }}
+        >
+          {/* TODO: Better key here */}
+          {chapters.map((chapter, index) => (
+            <PlaceholderMarkers
+              onClick={() =>
+                index !== selectedChapter && handleSelection(chapter, index)
+              }
+              selected={index === selectedChapter}
+              key={`chapter_${index}`}
+              title={chapter}
+              position={index * spacing}
+            />
+          ))}
+        </div>
+      </div>
+      <Tooltip effect={"solid"} />
+    </div>
+  );
+};
 
 const NavigationButtons = ({ back, next, onBack, onNext }) => {
   return (
     <div className={cs(classes.bookNav)}>
-      <button onClick={onBack} className={cs({ [classes.hideNav]: !back })}>
-        Back
+      <button
+        onClick={onBack}
+        className={cs(classes.navButton, { [classes.hideNav]: !back })}
+      >
+        <i className="icon-back"></i>
       </button>
-      <button onClick={onNext} className={cs({ [classes.hideNav]: !next })}>
-        Next
+      <button
+        onClick={onNext}
+        className={cs(classes.navButton, { [classes.hideNav]: !next })}
+      >
+        <i className="icon-next"></i>
       </button>
     </div>
   );
