1ed4927ffb4d9d0a9c170ae8e2f92da371909772
Zakaria Talhami <zakaria.a.a.talhami@gmail.com>
2020-12-06 00:51:32 +0200

    Feat: Added map tiles into the backend

--- backend/server/loaders/express.js
+++ backend/server/loaders/express.js
@@ -1,36 +1,35 @@
-import logger from 'morgan';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import createError from 'http-errors';
+import logger from "morgan";
+import express from "express";
+import cookieParser from "cookie-parser";
+import createError from "http-errors";
+import path from "path";
 import indexRouter from "../routes";
-import path from 'path'
 
 export default async ({ app }) => {
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "../../public")));
 
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, '../public')));
-    
-    //  Include the router
-    app.use("/", indexRouter);
-    
-    // catch 404 and forward to error handler
-    app.use(function (req, res, next) {
-        next(createError(404));
-    });
-    
-    // error handler
-    app.use(function (err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //  Include the router
+  app.use("/", indexRouter);
 
-        // render the error page
-        res.status(err.status || 500);
-        res.json({'error': err.message});
-    });
+  // catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
 
-    return app;
+  // error handler
+  app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+  });
+
+  return app;
 };
--- frontend/src/views/map/Map.js
+++ frontend/src/views/map/Map.js
@@ -1,21 +1,46 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+
+const PADDING = 10;
 
 const Map = () => {
   return (
     <MapContainer
       style={{ width: "100vw", height: "100vh" }}
       center={[4, 19]}
-      zoom={7}
+      zoom={3}
+      minZoom={3}
+      maxZoom={6}
       zoomControl={false}
       attributionControl={false}
+      maxBounds={[
+        [-10 - PADDING, -180 - PADDING],
+        [85 + PADDING, 157 + PADDING],
+      ]}
     >
       <TileLayer
-        // TODO: replace with wheel of time map
-        url="https://cartocdn-gusc.global.ssl.fastly.net/ramirocartodb/api/v1/map/named/tpl_756aec63_3adb_48b6_9d14_331c6cbc47cf/all/{z}/{x}/{y}.png"
+        url="http://localhost:8000/tiles/{z}/{y}/{x}.jpg"
+        noWrap={true}
+        bounds={[
+          [-10, -180],
+          [85, 157],
+        ]}
       />
+      <MapEvents />
     </MapContainer>
   );
 };
+
+function MapEvents() {
+  const map = useMapEvents({
+    click: (e) => {
+      console.log(e);
+    },
+    mousemove: (e) => {
+      console.log(e.latlng.lat.toFixed(2), e.latlng.lng.toFixed(2));
+    },
+  });
+  return null;
+}
 
 export default Map;
