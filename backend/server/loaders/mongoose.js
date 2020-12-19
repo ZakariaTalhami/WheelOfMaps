20406355b439b94d6ff4fce7d68f48056fce17b7
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2020-12-19 21:26:56 +0200

    Chore: add a .env example

--- backend/server/loaders/mongoose.js
+++ backend/server/loaders/mongoose.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 export default async () => {
     // should take the URL From the Env
+    const DB_HOST = process.env.DB_HOST;
+    const DB_PORT = process.env.DB_PORT;
 
     try {
-        await mongoose.connect("mongodb://localhost:27017", {
+        await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}`, {
             useCreateIndex: true,
             useNewUrlParser: true,
             useUnifiedTopology: true,
