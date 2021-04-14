98cb023443d8b351bc3c8020b7c4a52864b38716
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2021-04-14 22:55:30 +0300

    Fix: Drawer mobile style

--- frontend/src/components/sideMenu/drawer/Drawer.js
+++ frontend/src/components/sideMenu/drawer/Drawer.js
@@ -1,7 +1,23 @@
-import { Box, Flex, Icon, IconButton } from "@chakra-ui/react";
-import { FaTimes } from "react-icons/fa";
+// Core
 import React from "react";
+// Components
+import { FaTimes } from "react-icons/fa";
+import {
+    Box,
+    Flex,
+    Icon,
+    IconButton,
+    useBreakpointValue,
+} from "@chakra-ui/react";
+// Config
 import { NavigationType } from "../navigation";
+
+const DRAWER_STYLE = {
+    justify: "center",
+    bgColor: "primaryColor",
+    shadow: "14px -2px 10px 4px rgb(0 0 0 / 25%)",
+    transition: "all 0.5s",
+};
 
 const DrawerWraper = (props) => (
     <Flex
@@ -9,15 +25,28 @@ const DrawerWraper = (props) => (
         pos="absolute"
         top="0"
         left="100%"
-        justify="center"
-        bgColor="primaryColor"
         height="100%"
         minW="350px"
         w="30vw "
         maxW="600px"
-        shadow="14px -2px 10px 4px rgb(0 0 0 / 25%)"
-        transition="all 0.5s"
         transform={`translate(${props.open ? "0" : "-170%"})`}
+        {...DRAWER_STYLE}
+        {...props}
+    />
+);
+
+const MobileDrawerWrapper = (props) => (
+    <Flex
+        data-testid="side-drawer-mobile"
+        pos="fixed"
+        width="85%"
+        height="85%"
+        top={props.open ? "50%" : "150%"}
+        left="50%"
+        justify="center"
+        transform="translate(-50%, -50%)"
+        borderRadius="10px"
+        {...DRAWER_STYLE}
         {...props}
     />
 );
@@ -38,13 +67,18 @@ const DrawerContentWrapper = (props) => <Box w="100%" h="100%" {...props} />;
 
 const Drawer = ({ content, onClose }) => {
     const Content = content?.Content;
+
+    const isMobileView = useBreakpointValue({ base: true, sm: false });
+
+    const Drawer = isMobileView ? MobileDrawerWrapper : DrawerWraper;
+
     return (
-        <DrawerWraper open={!!content}>
+        <Drawer open={!!content}>
             <DrawerContentWrapper>
                 <DrawerHeader onClose={onClose} />
                 {Content && <Content />}
             </DrawerContentWrapper>
-        </DrawerWraper>
+        </Drawer>
     );
 };
 
